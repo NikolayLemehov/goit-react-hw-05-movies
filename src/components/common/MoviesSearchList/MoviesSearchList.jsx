0492aec14ed6626bc38ccd01593f2aedc2425363
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-09-05 21:30:44 +0300

    adds movies link to MoviesSearchList.jsx

--- src/components/common/MoviesSearchList/MoviesSearchList.jsx
+++ src/components/common/MoviesSearchList/MoviesSearchList.jsx
@@ -1,8 +1,13 @@
-export default function MoviesSearchList({movies}) {
+import { NavLink } from 'react-router-dom';
+import { useOriginPath } from '../../../hooks/useOriginPath';
 
+export default function MoviesSearchList({movies}) {
+  const originPath = useOriginPath();
   return (
     <ul>
-      {movies.map(({id, title}) => <li key={id}>{title}</li>)}
+      {movies.map(({id, title}) => (
+        <li key={id}><NavLink to={`${originPath}/${id}`}>{title}</NavLink></li>
+      ))}
     </ul>
   )
 }
