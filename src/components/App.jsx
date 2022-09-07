527250ddaa2df542d4e3deb70399f8229105168c
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-09-07 20:04:55 +0300

    adds lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
-import { ReactQueryDevtools } from 'react-query/devtools';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import s from './App.module.css';
-import Home from './pages/Home/Home';
-import Cast from './pages/Cast';
-import Movies from './pages/Movies';
-import Reviews from './pages/Reviews';
-import MovieDetails from './pages/MovieDetails';
-import NotFound from './pages/NotFound';
 import Header from './common/Header';
 import MoviesSearchValueCtx, { moviesSearchValueCtx } from '../context/MoviesSearchValue/MoviesSearchValueCtx';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./pages/Home'));
+const Cast = lazy(() => import('./pages/Cast'));
+const Movies = lazy(() => import('./pages/Movies'));
+const Reviews = lazy(() => import('./pages/Reviews'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 const queryClient = new QueryClient();
 
@@ -20,15 +22,17 @@ export const App = () => {
       <MoviesSearchValueCtx value={moviesSearchValueCtx}>
         <div className={s.container}>
           <Header />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/movies' element={<Movies />} />
-            <Route path='/movies/:movieId' element={<MovieDetails />}>
-              <Route path={'cast'} element={<Cast />} />
-              <Route path={'reviews'} element={<Reviews />} />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/movies' element={<Movies />} />
+              <Route path='/movies/:movieId' element={<MovieDetails />}>
+                <Route path={'cast'} element={<Cast />} />
+                <Route path={'reviews'} element={<Reviews />} />
+              </Route>
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </div>
       </MoviesSearchValueCtx>
       {/*<ReactQueryDevtools initialIsOpen={false} />*/}
